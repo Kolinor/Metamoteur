f7a3f44e989c067f71be555da7e62910d190a8dd
Kolinor <colin.mangas@gmail.com>
2022-03-14 18:03:09 +0100

    wip

--- srv/routes/recherches.js
+++ srv/routes/recherches.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const query = require('./lib/query');
 
 const recherche = async (req, res) => {
     try {
 
-        // console.log(req.pool);
-        res.send('test');
+        const res = await query({
+            sql: 'select * from sites;'
+        });
+        console.log(res);
     } catch(err) {
         console.error(err);
     }
