6c6c61e86d4375a1aff6a7ec4d32d86456f9a993
Kolinor <colin.mangas@gmail.com>
2022-04-14 15:32:52 +0200

    port 80

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000/",
+                url: "http://localhost:80/",
             },
         ],
     },
